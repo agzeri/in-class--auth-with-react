3bf04d21bcd23edc42d013b0c655e4613b292a64
Yair <hello@agzeri.com>
2018-04-24 11:52:03 -0500

    Configure routing in app.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
+
 class Public extends Component {
   render() {
     return <h3>Public</h3>
@@ -17,12 +24,19 @@ class Protected extends Component {
 class App extends Component {
   render() {
     return (
-      <div>
-        <ul>
-          <li>Public</li>
-          <li>Protected</li>
-        </ul>
-      </div>
+      <Router>
+        <div>
+          <ul>
+            <li><Link to='/public'>Public</Link></li>
+            <li><Link to='/protected'>Protected</Link></li>
+          </ul>
+
+          <Switch>
+            <Route exact path='/public' component={Public} />
+            <Route exact path='/Protected' component={Protected} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
